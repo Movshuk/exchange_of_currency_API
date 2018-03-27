f9f6b259cc1f0c38c4e5446f2a57b3f59d4f99d1
uber <uber@UBERPC>
2018-03-28 02:09:38 +0300

    Add class TypeMoney

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApplication1
 {
@@ -19,25 +20,94 @@ namespace ConsoleApplication1
 
          WebResponse response = request.GetResponse();
 
+         string line = null;
          
+         // чтение результата запроса в строку
          using (Stream stream = response.GetResponseStream())
          {
-            using (StreamReader reader = new StreamReader(stream))
-            {
-               
-               string line = "";
+
+            //using (StreamReader reader = new StreamReader(stream))
+            //{
+            //line = "";
+               StreamReader reader = new StreamReader(stream);
+               line = reader.ReadToEnd();
+            //Console.WriteLine(line);
+            //Console.WriteLine(line.Length + "*********!!!!!!!!!!!!!!!********");
+            /*
                while ((line = reader.ReadLine()) != null)
                {
                   Console.WriteLine(line);
+                  Console.WriteLine(line.Length + "*********!!!!!!!!!!!!!!!********");
                }
-               
-            }
+               */
+            //}
+            // здесь строки уже нет
          }
-         
 
+         
          response.Close();
          Console.WriteLine("Запрос выполнен");
          Console.Read();
+
+         // парс ответа
+         List<string> arrObj = new List<string>();
+
+         for (int i = 0; i < line.Length; i++)
+         {
+            if (line[i] == '{')
+            {
+               string strTmp = null;
+               i++;
+               for (int j = 0; line[i] != '}'; i++, j++)
+               {
+                  strTmp += line[i].ToString();
+               }
+               arrObj.Add(strTmp);
+            }
+         }
+
+         /*
+         int c = 1;
+         foreach (var l in arrObj)
+         {
+            Console.WriteLine(c.ToString() + l);
+            c++;
+         }
+         */
+
+         // формирование коллекции объектов
+         
+         TypeMoney tm;
+         List<TypeMoney> listTm = new List<TypeMoney>();
+
+         for (int i = 0; i < arrObj.Count; i++)
+         {
+            // создаем объект деньги
+            tm = new TypeMoney();
+
+            // извлекаем дату
+            string patternDate = @"\d{4}-\d{2}-\d{2}";
+            Regex reg = new Regex(patternDate);
+            Match parseDate = reg.Match(arrObj[i]);
+
+            tm.Date = parseDate.ToString();
+
+            // извлекаем абревеатуру валюты
+            string patternCurAbr = @"([A-Z]){3}";
+            Regex reg2 = new Regex(patternCurAbr);
+            Match parseCurAbr = reg2.Match(arrObj[i]);
+
+            tm.CurAbr = parseCurAbr.ToString();
+
+
+            listTm.Add(tm); // добавляю
+         }
+
+
+         foreach (TypeMoney t in listTm)
+         {
+            Console.WriteLine(t.Date + " " + t.CurAbr);
+         }
 
 
       }
---
+++ ConsoleApplication1/TypeMoney.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+   internal class TypeMoney
+   {
+      internal string Date { get; set; }
+      internal string CurAbr { get; set; }
+      internal int HowMany { get; set; }
+      internal string CurName { get; set; }
+      internal double CurRate { get; set; }
+
+   }
+}
